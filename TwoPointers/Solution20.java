f62963731003437b663d3120823ba4bee5bbb7e8
samwilson0745 <sohamdeb0745@gmail.com>
2024-06-08 21:02:22 +0530

    NC_Two_Pointer_Problem_20 solved

---
+++ TwoPointers/Solution20.java
@@ -0,0 +1,34 @@
+package DSA_Solved.TwoPointers;
+
+/*
+ * You are given an array of integers nums and an integer target.
+   Return the number of non-empty subsequences of nums such that the sum of the minimum and maximum element on it is less or equal to target. 
+   Since the answer may be too large, return it modulo 109 + 7.
+*/
+
+import java.util.Arrays;
+
+class Solution20{
+    public int numSubSeq(int[] nums, int target){
+        int mod = 1_000_000_007;
+        Arrays.sort(nums);
+        int n = nums.length;
+        int[] pow2 = new int[n+1];
+        pow2[0] = 1;
+        for(int i=1;i<=n;i++){
+            pow2[i] = (pow2[i-1] * 2) % mod;
+        }
+
+        int left = 0, right = n-1;
+        int result = 0;
+        while(left<=right){
+            if(nums[left] + nums[right]<=target){
+                result = (result + pow2[right-left]) % mod;
+                left++;
+            }else{
+                right--;
+            }
+        }
+        return result;
+    }
+}
